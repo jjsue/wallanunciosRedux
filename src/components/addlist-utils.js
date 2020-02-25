a30a798e9a36cc3e040fd6c1b8e968163c79b9e1
jjsue <jjsue_95@hotmail.es>
2020-02-25 12:32:08 +0100

    Muevo componentes relacionados con el mostrar anuncios a addlist-utils

---
+++ src/components/addlist-utils.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+class AddMount extends Component{
+    render(){
+      return(
+        <>
+        <div className="Anuncio">
+          <h2>{this.props.data.name}</h2>
+          <img src={this.props.data.photo} alt={this.props.data.name}/> 
+          <p>Precio: {this.props.data.price}</p>
+          <h3>Descripción:</h3>
+          <p>{this.props.data.description}</p>
+        </div>
+        </>
+      )
+    }
+  }
+  
+  class AddMount3 extends Component{
+    render(){
+      return null;
+    }
+  }
+  
+  function RenderOrNot(props) {
+    const callDo = props.callDo;
+    let toShow = [];
+    if (callDo) {
+      for (let i=0; i<props.data.count; i++){
+        toShow.push(<AddMount key = {i} data = {props.data.results[i]}/>);
+      }
+      console.log(toShow);
+      return(
+      <div className="Anuncios">
+        {toShow}
+      </div>
+      )
+    }
+    return <AddMount3 />;
+  }
+
+export {RenderOrNot};
--- src/components/addlist.js
+++ src/components/addlist.js
@@ -1,43 +1,7 @@
 import React, { Component } from 'react';
 import {addCall} from './../api-calls/api-calls';
-class AddMount extends Component{
-  render(){
-    return(
-      <>
-      <div className="Anuncio">
-        <h2>{this.props.data.name}</h2>
-        <img src={this.props.data.photo} alt={this.props.data.name}/> 
-        <p>Precio: {this.props.data.price}</p>
-        <h3>Descripción:</h3>
-        <p>{this.props.data.description}</p>
-      </div>
-      </>
-    )
-  }
-}
+import {RenderOrNot} from './addlist-utils';
 
-class AddMount3 extends Component{
-  render(){
-    return null;
-  }
-}
-
-function RenderOrNot(props) {
-  const callDo = props.callDo;
-  let toShow = [];
-  if (callDo) {
-    for (let i=0; i<props.data.count; i++){
-      toShow.push(<AddMount key = {i} data = {props.data.results[i]}/>);
-    }
-    console.log(toShow);
-    return(
-    <div className="Anuncios">
-      {toShow}
-    </div>
-    )
-  }
-  return <AddMount3 />;
-}
 export default class AdsList extends Component{
   constructor(props){
     super(props);
